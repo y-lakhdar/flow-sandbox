a2137acf9f4efa329fa32843318796b320d705b3
y-lakhdar <ylakhdar@coveo.com>
2018-12-02 13:03:43 -0500

    Text next-version script

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+<a name="1.16.1"></a>
+## [1.16.1](https://github.com/y-lakhdar/flow-sandbox/compare/v1.16.0...v1.16.1) (2018-12-02)
---
+++ get-next-version.js
@@ -0,0 +1,32 @@
+const packageJson = require('./package.json');
+const conventionalRecommendedBump = require('conventional-recommended-bump');
+const semver = require('semver');
+
+const getNextVersion = currentVersion => {
+  return new Promise((resolve, reject) => {
+    conventionalRecommendedBump(
+      {
+        preset: 'angular'
+      },
+      (err, release) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        console.log('*********************');
+        console.log(currentVersion);
+        console.log(release.releaseType);
+        console.log('*********************');
+        
+        const nextVersion = semver.valid(release.releaseType) || semver.inc(currentVersion, release.releaseType);
+
+        resolve(nextVersion);
+      }
+    );
+  });
+};
+
+getNextVersion(packageJson.version)
+  .then(version => console.log(version))
+  .catch(error => console.log(error));
