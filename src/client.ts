b1ca9c852af945a3415e35fb43539d2c5dbf6190
y-lakhdar <ylakhdar@coveo.com>
2018-12-02 16:16:46 -0500

    fix: Remove extra console logs
    fixes #1 and #2

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,5 @@
 const pkg: any = require('./../package.json');
 
 console.log('--------------------');
-console.log('--------------------');
 console.log(pkg.version);
-console.log('---------------------');
 console.log('---------------------');
