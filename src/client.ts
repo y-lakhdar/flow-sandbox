75cdf91d7546b00377fb204bd0d113935e2a930c
Yassine Lakhdar <ylakhdar@coveo.com>
2019-11-21 18:14:09 -0500

    fix: remove console log

--- src/client.ts
+++ src/client.ts
@@ -3,10 +3,9 @@ const pkg: any = require('./../package.json');
 const updateNotifier = require('update-notifier');
 updateNotifier({ pkg }).notify();
 
-console.log('*********************');
-console.log(pkg.version);
-console.log('*********************');
-
 program
   .option('--env [value]', 'Environment (Production by default. Supported environments are: development|qa|production)')
   .version(pkg.version);
+
+// Parsing the arguments
+program.parse(process.argv);
