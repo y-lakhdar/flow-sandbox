e476646deda71eee3336ca244f32b7094dd20f48
Yassine Lakhdar <ylakhdar@coveo.com>
2019-11-21 17:55:02 -0500

    feat: add console log

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,10 @@ const pkg: any = require('./../package.json');
 const updateNotifier = require('update-notifier');
 updateNotifier({ pkg }).notify();
 
+console.log('*********************');
+console.log(pkg.version);
+console.log('*********************');
+
 program
   .option('--env [value]', 'Environment (Production by default. Supported environments are: development|qa|production)')
   .version(pkg.version);
