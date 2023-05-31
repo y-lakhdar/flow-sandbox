526e766bbfdf7e2705f918847238c1e140d25158
ylakhdar <ylakhdar@coveo.com>
2023-05-31 14:19:29 -0400

    clean

--- gulpTasks/compile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-const gutil = require('gulp-util');
-
-// Transpiling the source files into the bin/ folder
-gulp.task('compile', shell.task([`webpack --process --progress --colors ${gutil.env.minimize ? '--minimize' : ''}`]));
-
-gulp.task('compileForDev', shell.task([`webpack --process --colors --dev-mode`]));
